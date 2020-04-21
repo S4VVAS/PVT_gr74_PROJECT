17d55788ed5da6ddc8b8a2ba2ad79af930d5e721
knopp10000 <karl.gustafsson@live.com>
2020-04-21 13:48:09 +0200

    main

--- src/main/java/com/group74/HistoryGoServer/HistoryGoServerApplication.java
+++ src/main/java/com/group74/HistoryGoServer/HistoryGoServerApplication.java
@@ -2,12 +2,24 @@ package com.group74.HistoryGoServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
-public class HistoryGoServerApplication {
+@RestController
+public class HistoryGoServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HistoryGoServerApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value ="name", defaultValue ="World")String name) {
+		return String.format("Hello %s", name);
+
 	}
 
 }
