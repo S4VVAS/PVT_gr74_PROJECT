a80ae2830fb0056115cb69fbdc838848dab0dc67
Savvas <savvasswe@gmail.com>
2020-05-04 12:22:31 +0200

    JSON dl works

--- src/main/java/com/group74/HistoryGoServer/HistoryGoServerApplication.java
+++ src/main/java/com/group74/HistoryGoServer/HistoryGoServerApplication.java
@@ -1,31 +1,75 @@
 package com.group74.HistoryGoServer;
 
+
+import java.io.File;
+
+import org.json.simple.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @SpringBootApplication
 @RestController
 public class HistoryGoServerApplication extends SpringBootServletInitializer {
+	
+	static JSONObject[] JSONArr;
 
+	
 	public static void main(String[] args) {
+		JSONObject obj = new JSONObject();
+		obj.put("JSON", "OBJ");
+		JSONArr = new JSONObject[] {obj};
 		SpringApplication.run(HistoryGoServerApplication.class, args);
 	}
 
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value ="name", defaultValue ="World")String name) {
-		return String.format("Hello %s", name);
+	public JSONObject hello(@RequestParam(value ="name", defaultValue ="World")String name) {
+		return JSONArr[0];
 
 	}
 	
-	@GetMapping("/bruh")
-	public String bruh(@RequestParam(value ="name", defaultValue ="bruh")String name) {
-		return String.format("Sup %s", name);
+	@PostMapping("/upload")
+	public String handleFileUpload(@RequestParam("file") MultipartFile file,
+			RedirectAttributes redirectAttributes) {
 
+		redirectAttributes.addFlashAttribute("message",
+				"You successfully uploaded " + file.getOriginalFilename() + "!");
+
+		return "redirect:/hello";
 	}
 
 }
+
+
+/*
+	//LADDA UPP
+ * @PostMapping("/")
+	public String handleFileUpload(@RequestParam("file") MultipartFile file,
+			RedirectAttributes redirectAttributes) {
+
+		storageService.store(file);
+		redirectAttributes.addFlashAttribute("message",
+				"You successfully uploaded " + file.getOriginalFilename() + "!");
+
+		return "redirect:/";
+	}
+	
+	//LADDA NER
+	@GetMapping("/")
+	public String listUploadedFiles(Model model) throws IOException {
+
+		model.addAttribute("files", storageService.loadAll().map(
+				path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
+						"serveFile", path.getFileName().toString()).build().toUri().toString())
+				.collect(Collectors.toList()));
+
+		return "uploadForm";
+	}
+*/
